87b0ee34801f2bc56617a3470a26a81b5bc0bd01
Michelle rogers <Cuddlemeplatonically@gmail.com>
2026-09-17 07:44:45 -0700

    security: add bounded auth-page error helper

---
+++ src/lib/safe-auth-page-error.js
@@ -0,0 +1,7 @@
+const SAFE_AUTH_PAGE_ERROR = "We couldn't sign you in. Check your details and try again.";
+
+export function safeAuthPageError() {
+  return SAFE_AUTH_PAGE_ERROR;
+}
+
+export { SAFE_AUTH_PAGE_ERROR };
